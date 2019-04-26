0e3b177646f8daadece84aa1f8144a2066bb9c3a
4421_羽坂 公佑 <a>
2019-04-26 10:22:40 +0900

    20190426

--- client.c
+++ client.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h> 
+#include <string.h>
+#include <unistd.h>
+
+int commun(int sock)
+{
+	int len_r;
+	char buf[256];
+	char *message = "冷蔵庫の国のココモン";
+	
+	send(sock, message, strlen(message),0);
+	len_r = recv(sock,buf,256,0);
+	buf[len_r] = '\0';
+	printf("%s\n",buf);
+}
 
 int main(int argc, char **argv)
 {
@@ -10,6 +25,7 @@ int main(int argc, char **argv)
 	target.sin_port = htons(10001);
 	
 	connect(sock, (struct sockaddr*)&target, sizeof(target));
+	commun(sock);
 	close(sock);
 	return 0;
 }
