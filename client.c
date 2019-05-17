f0b1012daeced6d22b2bcd04d5aa5f5305432619
shigu77 <49297397+shigu77@users.noreply.github.com>
2019-05-17 09:17:03 +0900

    Update client.c

--- client.c
+++ client.c
@@ -1,30 +1,42 @@
-#include <stdio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h> 
-#include <string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-int commun(int sock)
-{
-	int len_r;
-	char buf[256];
-	char *message = "冷蔵庫の国のココモン";
-	
-	send(sock, message, strlen(message),0);
-	len_r = recv(sock,buf,256,0);
-	buf[len_r] = '\0';
-	printf("%s\n",buf);
+#define BUF_SIZE 256
+
+void DieWithError(char *errorMessage){
+	perror(errorMessage);
+	exit(1);
 }
 
-int main(int argc, char **argv)
-{
-	int sock = socket(PF_INET, SOCK_STREAM, 0);
+void commun(int sock){
+	char buf[BUF_SIZE];
+	int len_r;
+	char*message = "!!";
+	if(send(sock,message,strlen(message),0)!= strlen(message))
+	DieWithError("send() set a message of unexpected bytes");
+	if((len_r=recv(sock,buf,BUF_SIZE,0) <= 0))
+	DieWithError("recv()failed");
+	buf[len_r] = '\0';
+	printf("%s\n",buf);
+	return ;
+}
+
+int main(int argc, char** argv){
+	if (argc !=3)DieWithError("arguments is not available");
+	int sock = socket(PF_INET,SOCK_STREAM, 0);
+	if(sock < 0) DieWithError("socket()failed");
+	char *server_ipaddr = argv[1];// "10.13.64.20"
+	int server_port = 10001;
 	struct sockaddr_in target;
 	target.sin_family = AF_INET;
-	target.sin_addr.s_addr = inet_addr("10.13.64.20");
-	target.sin_port = htons(10001);
-	
-	connect(sock, (struct sockaddr*)&target, sizeof(target));
+	target.sin_addr.s_addr = inet_addr(server_ipaddr);
+	target.sin_port = htons(server_port);
+	if(connect(sock,(struct sockaddr*)&target, sizeof(target))<0)
+	DieWithError("connect()failed");
 	commun(sock);
 	close(sock);
 	return 0;
