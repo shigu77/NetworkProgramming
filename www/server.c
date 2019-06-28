2ebc500eb34b2df1ad21334d1f28a9cb62cd50f4
shigu77 <k16156kh@apps.kct.ac.jp>
2019-06-28 10:28:27 +0900

    2019/06/28

--- www/server.c
+++ www/server.c
@@ -9,23 +9,25 @@
 
 void commun(int);
 void DieWithError(char *);
-int main(int argc,char** argv){
+int main(int argc,char** argv)
+{
 	
 	int cliSock;
 	int servSock = socket(PF_INET,SOCK_STREAM,0);
 	struct sockaddr_in servAddress;
 	struct sockaddr_in clientAddress;
 	socklen_t szClientAddr;
-	servAddress.sin_family = AF_INET;
-	servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-	servAddress.sin_port = htons(80);
+	servAddress.sin_family		 = AF_INET;
+	servAddress.sin_addr.s_addr	 = htonl(INADDR_ANY);
+	servAddress.sin_port		 = htons(80);
 	
-	bind(servSock,(struct sockaddr *)&servAddress,sizeof(servAddress));
+	bind(servSock, (struct sockaddr *) & servAddress, sizeof(servAddress));
 	
 	listen(servSock,5);
-	while(1){
+	while(1)
+	{
 		szClientAddr = sizeof(clientAddress);
-		cliSock = accept(servSock,(struct sockaddr *)&clientAddress,&szClientAddr);
+		cliSock		 = accept(servSock,(struct sockaddr *)&clientAddress,&szClientAddr);
 		
 		/*--------------------*/
 		/*		í êMäJén	  */
@@ -38,15 +40,40 @@ int main(int argc,char** argv){
 	return 0;
 }
 
-void DieWithError(char *errorMessage){
+void DieWithError(char *errorMessage)
+{
 	perror(errorMessage);
 	exit(1);
 }
-void commun(int sock){
+void commun(int sock)
+{
 	char buf[BUF_SIZE];
 	int len_r;
-	if((len_r=recv(sock,buf,BUF_SIZE,0))<=0)DieWithError("recv()failed");
-	buf[len_r]='\0';
+	char response[BUF_SIZE];
+	
+	while(0 < (len_r=recv(sock,buf,BUF_SIZE,0)))
+	{
+		buf[len_r]='\0';
+		printf("%s\n",buf);
+		if(strstr(buf, "\r\n\r\n"))
+		{
+			break;
+		}
+	}
+	
+	printf("kaigyou futatu atta yo");
+	
+	if(len_r <= 0)
+	{
+		DieWithError("recv()failed");
+	}
+	
 	printf("%s\n",buf);
-	if(send(sock,buf,strlen(buf),0)!=strlen(buf))DieWithError("send() sent amessage of unexpected bytes");
+	
+	sprintf(response, "HTTP/1.1 200 OK\r\n");
+	
+	if(send(sock,response,strlen(response),0)!=strlen(response))
+	{
+		DieWithError("send() sent amessage of unexpected bytes");
+	}
 }
