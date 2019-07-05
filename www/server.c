0375f1aeab351d34bd4bd95089d3cb66c5aaa7b5
shigu77 <k16156kh@apps.kct.ac.jp>
2019-07-05 10:18:45 +0900

    2019/07/05

--- www/server.c
+++ www/server.c
@@ -48,17 +48,30 @@ void DieWithError(char *errorMessage)
 void commun(int sock)
 {
 	char buf[BUF_SIZE];
+	char buf2[BUF_SIZE * 2];
+	char buf_old[BUF_SIZE];
 	int len_r;
 	char response[BUF_SIZE];
 	
+	buf_old[0] = '/0';
+
 	while(0 < (len_r=recv(sock,buf,BUF_SIZE,0)))
 	{
 		buf[len_r]='\0';
-		printf("%s\n",buf);
-		if(strstr(buf, "\r\n\r\n"))
+		sprintf(buf2, "%s%s", buf_old, buf);
+		
+		//printf("%s\n",buf);
+		/*if(strstr(buf, "\r\n\r\n"))
+		{
+			break;
+		}*/
+		
+		if(strstr(buf2, "\r\n\r\n"))
 		{
 			break;
 		}
+		
+		sprintf(buf_old, "%s", buf);
 	}
 	
 	printf("kaigyou futatu atta yo");
